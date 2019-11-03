1f628b0f02250ff2be59261b13c95062ba984a70
Li SH <li012589li@outlook.com>
2019-11-03 20:43:56 +0800

    add background color

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -2,4 +2,10 @@
 (setq TeX-parse-self t)
 (setq-default TeX-master nil)
 
+(setq reftex-plug-into-AUCTeX t)
+;; (setq preview-transparent-color 20)
+(set-default 'preview-scale-function 1.5)
+
+(custom-set-faces '(preview-reference-face ((t (:background "gray100")))))
+
 (provide 'init-auctex)
